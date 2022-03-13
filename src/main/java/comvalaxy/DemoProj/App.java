839b9079dd20bd15d8a7e9bcf8f967324fee3ac8
Your Name <beeraka.lakshman45@gmail.com>
2022-03-13 11:07:16 +0530

    initial commit

---
+++ src/main/java/comvalaxy/DemoProj/App.java
@@ -0,0 +1,13 @@
+package comvalaxy.DemoProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is lakshman" );
+    }
+}
